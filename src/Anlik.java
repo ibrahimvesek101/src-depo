40a3c18186967d51e04fd553ba8898a94b520334
ibrahimvesek101 <ibrahimvesek101@gmail.com>
2023-03-07 09:50:55 +0300

    7mart

--- src/Anlik.java
+++ src/Anlik.java
@@ -8,16 +8,6 @@ import static java.util.Arrays.sort;
 
 public class Anlik {
     public static void main(String[] args) {
-        /* TASK :
-
-
-         * Input olarak verilen listteki isimlerden
-         * icinde 'a' ve 'A' harfi bulunanlari silen bir code create ediniz.
-         *
-         * INPUT : list1={"Ali","Veli","Ayse","Fatma","Omer"}
-         * OUTPUT : [Veli,Omer]
-         */
-
 
 
     }
