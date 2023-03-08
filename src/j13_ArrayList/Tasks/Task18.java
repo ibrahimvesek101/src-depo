2cb9bdd6c16a55b8935dee2928c76cb9fa13273b
ibrahimvesek101 <ibrahimvesek101@gmail.com>
2023-03-08 09:48:07 +0300

    8_mart

---
+++ src/j13_ArrayList/Tasks/Task18.java
@@ -0,0 +1,16 @@
+package j13_ArrayList.Tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task18 {
+    public static void main(String[] args) {
+        /* Task->  Verilen bir listede, istenen iki index'deki elementlerin yerini
+         kalici olarak degistiren bir method create ediniz
+
+         */
+
+
+
+    }
+}
